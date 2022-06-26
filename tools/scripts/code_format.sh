d361fe1a22becd98ff7991bc7b0ab366a1bd0676
nearu <zynearu@gmail.com>
2022-06-26 17:57:32 +0800

    fp: modify code_format.sh

--- tools/scripts/code_format.sh
+++ tools/scripts/code_format.sh
@@ -15,6 +15,5 @@ fi
 
 astyle_bin=$base_dir/tools/astyle/${os}/astyle_${os}
 astyle_options=$base_dir/tools/astyle/google.ini
-astyle="${astyle_bin} --options=${astyle_options} -n -v -z1 ${base_dir}/src/*.cc ${base_dir}/src/base_module/fp_log/*.cc ${base_dir}/src/common/*.h ${base_dir}/src/base_module/fp_log/*.h"
-
+astyle="${astyle_bin} --options=${astyle_options} -n -v -z1 -r ${base_dir}/src/*.cc,*.h"
 ${astyle}
